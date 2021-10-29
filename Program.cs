5599c4264215a9f071e461f5696625e13e2a204c
kaiorod <caiorodrigo2775@gmail.com>
2021-10-29 19:52:46 -0300

    Finaliza CalculadoraIMC

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace CalculadoraIMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double massa, altura, imc; 
+
+            Console.Beep();
+            Console.Clear();
+
+            Console.Write("Digite seu peso (kg) : ");
+            massa = double.Parse(Console.ReadLine()); 
+
+            Console.Write("Digite sua altura (m) : ");
+            altura = double.Parse(Console.ReadLine()); 
+
+            imc = massa / Math.Pow(altura, 2); 
+            
+            if(imc < 17)
+            {
+                Console.WriteLine($"\nSeu imc é de {imc:N2} kg/m²");
+                Console.WriteLine("Diagnóstoco: Muito abaixo do peso");
+            }
+            else if(imc >= 17 && imc < 18.5)
+            {
+                Console.WriteLine($"\nSeu imc é de {imc:N2} kg/m²");
+                Console.WriteLine("Diagnóstoco: Abaixo do peso");
+            }
+            else if(imc >= 18.5 && imc < 25)
+            {
+                Console.WriteLine($"\nSeu imc é de {imc:N2} kg/m²");
+                Console.WriteLine("Diagnóstoco: Peso normal");
+            }
+            else if(imc >= 25 && imc < 30)
+            {
+                Console.WriteLine($"\nSeu imc é de {imc:N2} kg/m²");
+                Console.WriteLine("Diagnóstoco: Acima do peso");
+            }
+            else if(imc >= 30 && imc < 35)
+            {
+                Console.WriteLine($"\nSeu imc é de {imc:N2} kg/m²");
+                Console.WriteLine("Diagnóstoco: Obesidade I");
+            }
+            else if(imc >= 35 && imc < 40)
+            {
+                Console.WriteLine($"\nSeu imc é de {imc:N2} kg/m²");
+                Console.WriteLine("Diagnóstoco: Obesidade II (severa)");
+            }
+            else if(imc >= 40)
+            {
+                Console.WriteLine($"\nSeu imc é de {imc:N2} kg/m²");
+                Console.WriteLine("Diagnóstoco: Obesidade III (mórbida)");
+            }
+
+        
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # CalculadoraIMC
 Ex.:  -- Calculadora de IMC --  Digite seu peso em kg...: 90 Digite sua altura em m..: 1,75  Seu IMC é 29,39 kg/m². Diagnóstico: Acima do peso
+
+Resultado	Situação
+Abaixo de 17	Muito abaixo do peso
+Entre 17 e 18,49	Abaixo do peso
+Entre 18,5 e 24,99	Peso normal
+Entre 25 e 29,99	Acima do peso
+Entre 30 e 34,99	Obesidade I
+Entre 35 e 39,99	Obesidade II (severa)
+Acima de 40	Obesidade III (mórbida)
